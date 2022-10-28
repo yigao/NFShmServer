5e6e9529b09e1bfa7a6f9c4aa33631025990104a
gaoyi445 <445267987@qq.com>
2022-10-28 22:10:10 +0800

    work server

---
+++ src/NFServerComm/NFServerCommon/NFTransMsgServerModule.cpp
@@ -0,0 +1,315 @@
+// -------------------------------------------------------------------------
+//    @FileName         :    NFTransMsgServerModule.cpp
+//    @Author           :    gaoyi
+//    @Date             :    22-10-28
+//    @Email			:    445267987@qq.com
+//    @Module           :    NFTransMsgServerModule
+//
+// -------------------------------------------------------------------------
+#include "NFTransMsgServerModule.h"
+#include "NFComm/NFPluginModule/NFIConfigModule.h"
+#include "NFServerCommonDefine.h"
+#include "NFIServerMessageModule.h"
+#include "NFComm/NFPluginModule/NFIMonitorModule.h"
+#include "NFComm/NFPluginModule/NFINamingModule.h"
+#include "NFComm/NFPluginModule/NFCheck.h"
+
+#define SERVER_CONNECT_MASTER_SERVER "Server Connect MasterServer"
+
+#define SERVER_REPORT_TO_MASTER_SERVER_TIMER_ID 101
+#define SERVER_SERVER_DEAD_TIMER_ID 102
+
+bool NFTransMsgServerModule::IsConnectMasterServer() const
+{
+    return m_connectMasterServer;
+}
+
+void NFTransMsgServerModule::SetConnectMasterServer(bool connectMasterServer)
+{
+    m_connectMasterServer = connectMasterServer;
+}
+
+int NFTransMsgServerModule::BindServer()
+{
+    NFServerConfig *pConfig = FindModule<NFIConfigModule>()->GetAppConfig(m_serverType);
+    CHECK_EXPR_ASSERT(pConfig, -1, "GetAppConfig Failed, server type:{}", m_serverType);
+
+    //////////////////////master msg//////////////////////////
+    FindModule<NFIMessageModule>()->AddMessageCallBack(m_serverType, proto_ff::NF_MASTER_SERVER_SEND_OTHERS_TO_SERVER, this,
+                                                       &NFTransMsgServerModule::OnHandleServerReportFromMasterServer);
+
+    if (!m_pObjPluginManager->IsLoadAllServer())
+    {
+        CHECK_EXPR_ASSERT(pConfig->ServerType == m_serverType, -1, "server config error, server id not match the server type!:{}", m_serverType);
+    }
+
+    //注册要完成的服务器启动任务
+    if (m_connectMasterServer)
+    {
+        m_pObjPluginManager->RegisterAppTask(m_serverType, APP_INIT_CONNECT_MASTER,
+                                             NF_FORMAT("{}_{}", pConfig->ServerName, SERVER_CONNECT_MASTER_SERVER));
+    }
+
+    uint64_t serverLinkId = FindModule<NFIMessageModule>()->BindServer(m_serverType, pConfig->Url, pConfig->NetThreadNum, pConfig->MaxConnectNum,
+                                                                       PACKET_PARSE_TYPE_INTERNAL);
+    CHECK_EXPR_ASSERT(serverLinkId > 0, -1, "Server:{} Listen Failed, ServerId:{}, Ip:{}, Port:{}", pConfig->ServerName, pConfig->ServerId,
+                      pConfig->ServerIp, pConfig->ServerPort);
+
+    FindModule<NFIMessageModule>()->SetServerLinkId(m_serverType, serverLinkId);
+    FindModule<NFIMessageModule>()->AddEventCallBack(m_serverType, serverLinkId, this, &NFTransMsgServerModule::OnServerSocketEvent);
+    FindModule<NFIMessageModule>()->AddOtherCallBack(m_serverType, serverLinkId, this,
+                                                     &NFTransMsgServerModule::OnHandleServerOtherMessage);
+    NFLogInfo(NF_LOG_SYSTEMLOG, 0, "Server:{} Listen Success, ServerId:{}, Ip:{}, Port:{}", pConfig->ServerName, pConfig->ServerId, pConfig->ServerIp,
+              pConfig->ServerPort);
+
+    Subscribe(proto_ff::NF_EVENT_SERVER_DEAD_EVENT, 0, proto_ff::NF_EVENT_SERVER_TYPE, __FUNCTION__);
+    Subscribe(proto_ff::NF_EVENT_SERVER_APP_FINISH_INITED, m_serverType, proto_ff::NF_EVENT_SERVER_TYPE, __FUNCTION__);
+
+    SetTimer(SERVER_REPORT_TO_MASTER_SERVER_TIMER_ID, 10000);
+    return 0;
+}
+
+int NFTransMsgServerModule::OnExecute(uint32_t nEventID, uint64_t nSrcID, uint32_t bySrcType, const google::protobuf::Message &message)
+{
+    if (bySrcType == proto_ff::NF_EVENT_SERVER_TYPE)
+    {
+        if (nEventID == proto_ff::NF_EVENT_SERVER_DEAD_EVENT)
+        {
+            SetTimer(SERVER_SERVER_DEAD_TIMER_ID, 10000, 0);
+        } else if (nEventID == proto_ff::NF_EVENT_SERVER_APP_FINISH_INITED)
+        {
+            RegisterMasterServer(proto_ff::EST_NARMAL);
+        }
+    }
+
+    Subscribe(proto_ff::NF_EVENT_SERVER_DEAD_EVENT, 0, proto_ff::NF_EVENT_SERVER_TYPE, __FUNCTION__);
+    return 0;
+}
+
+void NFTransMsgServerModule::OnTimer(uint32_t nTimerID)
+{
+    if (nTimerID == SERVER_REPORT_TO_MASTER_SERVER_TIMER_ID)
+    {
+        ServerReportToMasterServer();
+    } else if (nTimerID == SERVER_SERVER_DEAD_TIMER_ID)
+    {
+        NFLogError(NF_LOG_SYSTEMLOG, 0, "kill the exe..................");
+        NFSLEEP(1000);
+        exit(0);
+    }
+}
+
+int NFTransMsgServerModule::ConnectMasterServer()
+{
+    if (m_connectMasterServer == false)
+    {
+        return 0;
+    }
+
+
+    NFServerConfig *pConfig = FindModule<NFIConfigModule>()->GetAppConfig(m_serverType);
+    CHECK_EXPR_ASSERT(pConfig, -1, "GetAppConfig Failed, server type:{}", m_serverType);
+
+#if NF_PLATFORM == NF_PLATFORM_WIN
+    proto_ff::ServerInfoReport masterData = FindModule<NFINamingModule>()->GetDefaultMasterInfo(m_serverType);
+    int32_t ret = ConnectMasterServer(masterData);
+    CHECK_EXPR(ret == 0, false, "ConnectMasterServer Failed, url:{}", masterData.DebugString());
+#else
+    if (pConfig->RouteConfig.NamingHost.empty())
+    {
+        proto_ff::ServerInfoReport masterData = FindModule<NFINamingModule>()->GetDefaultMasterInfo(m_serverType);
+        int32_t ret = ConnectMasterServer(masterData);
+        CHECK_EXPR(ret == 0, -1, "ConnectMasterServer Failed, url:{}", masterData.DebugString());
+    }
+#endif
+
+    FindModule<NFINamingModule>()->WatchTcpUrls(m_serverType, NF_ST_MASTER_SERVER,
+                                                [this](const string &name, const proto_ff::ServerInfoReport &xData, int32_t errCode)
+                                                {
+                                                    if (errCode != 0)
+                                                    {
+                                                        NFLogError(NF_LOG_SYSTEMLOG, 0,
+                                                                   "Server Watch, MasterServer Dump, errCode:{} name:{} serverInfo:{}", errCode, name,
+                                                                   xData.DebugString());
+
+                                                        return;
+                                                    }
+                                                    NFLogInfo(NF_LOG_SYSTEMLOG, 0, "Server Watch MasterServer name:{} serverInfo:{}", name,
+                                                              xData.DebugString());
+
+                                                    int32_t ret = ConnectMasterServer(xData);
+                                                    CHECK_EXPR(ret == 0, , "ConnectMasterServer Failed, url:{}", xData.DebugString());
+                                                });
+
+    return 0;
+}
+
+int NFTransMsgServerModule::OnServerSocketEvent(eMsgType nEvent, uint64_t unLinkId)
+{
+    NFLogTrace(NF_LOG_SYSTEMLOG, 0, "-- begin --");
+    if (nEvent == eMsgType_CONNECTED)
+    {
+
+    } else if (nEvent == eMsgType_DISCONNECTED)
+    {
+        OnHandleServerDisconnect(unLinkId);
+    }
+    NFLogTrace(NF_LOG_SYSTEMLOG, 0, "-- end --");
+    return 0;
+}
+
+int NFTransMsgServerModule::OnHandleServerOtherMessage(uint64_t unLinkId, NFDataPackage &packet)
+{
+    NFLogWarning(NF_LOG_SYSTEMLOG, 0, "msg:{} not handled!", packet.ToString());
+    return 0;
+}
+
+int NFTransMsgServerModule::OnHandleServerDisconnect(uint64_t unLinkId)
+{
+    NFServerConfig *pConfig = FindModule<NFIConfigModule>()->GetAppConfig(m_serverType);
+    CHECK_EXPR_ASSERT(pConfig, false, "GetAppConfig Failed, server type:{}", m_serverType);
+
+    NF_SHARE_PTR<NFServerData> pServerData = FindModule<NFIMessageModule>()->GetServerByUnlinkId(m_serverType, unLinkId);
+    if (pServerData)
+    {
+        pServerData->mServerInfo.set_server_state(proto_ff::EST_CRASH);
+        pServerData->mUnlinkId = 0;
+
+        NFLogError(NF_LOG_SYSTEMLOG, 0, "the server:{} disconnect from server:{}, serverName:{}, busid:{}, serverIp:{}, serverPort:{}",
+                   pServerData->mServerInfo.server_name(), pConfig->ServerName, pServerData->mServerInfo.server_name(),
+                   pServerData->mServerInfo.bus_id(), pServerData->mServerInfo.server_ip(), pServerData->mServerInfo.server_port());
+    }
+
+    FindModule<NFIMessageModule>()->DelServerLink(m_serverType, unLinkId);
+    return 0;
+}
+
+int NFTransMsgServerModule::ConnectMasterServer(const proto_ff::ServerInfoReport &xData)
+{
+    NFServerConfig *pConfig = FindModule<NFIConfigModule>()->GetAppConfig(m_serverType);
+    CHECK_EXPR_ASSERT(pConfig, false, "GetAppConfig Failed, server type:{}", m_serverType);
+    auto pMsterServerData = FindModule<NFIMessageModule>()->GetMasterData(m_serverType);
+    if (pMsterServerData->mUnlinkId <= 0)
+    {
+        pMsterServerData->mUnlinkId = FindModule<NFIMessageModule>()->ConnectServer(m_serverType, xData.url(), PACKET_PARSE_TYPE_INTERNAL);
+        FindModule<NFIMessageModule>()->AddEventCallBack(m_serverType, pMsterServerData->mUnlinkId, this, &NFTransMsgServerModule::OnMasterSocketEvent);
+        FindModule<NFIMessageModule>()->AddOtherCallBack(m_serverType, pMsterServerData->mUnlinkId, this,
+                                                         &NFTransMsgServerModule::OnHandleMasterOtherMessage);
+    }
+
+    pMsterServerData->mServerInfo = xData;
+
+    return 0;
+}
+
+int NFTransMsgServerModule::RegisterMasterServer(uint32_t serverState)
+{
+    if (m_connectMasterServer == false)
+    {
+        return 0;
+    }
+
+    NFServerConfig *pConfig = FindModule<NFIConfigModule>()->GetAppConfig(m_serverType);
+    if (pConfig)
+    {
+        proto_ff::ServerInfoReportList xMsg;
+        proto_ff::ServerInfoReport *pData = xMsg.add_server_list();
+        NFServerCommon::WriteServerInfo(pData, pConfig);
+
+        pData->set_server_state(serverState);
+        FindModule<NFIServerMessageModule>()->SendMsgToMasterServer(m_serverType, proto_ff::NF_SERVER_TO_SERVER_REGISTER, xMsg);
+    }
+    return 0;
+}
+
+/*
+	处理Master服务器链接事件
+*/
+int NFTransMsgServerModule::OnMasterSocketEvent(eMsgType nEvent, uint64_t unLinkId)
+{
+    NFServerConfig *pConfig = FindModule<NFIConfigModule>()->GetAppConfig(m_serverType);
+    CHECK_EXPR_ASSERT(pConfig, false, "GetAppConfig Failed, server type:{}", m_serverType);
+
+    if (nEvent == eMsgType_CONNECTED)
+    {
+        std::string ip = FindModule<NFIMessageModule>()->GetLinkIp(unLinkId);
+        NFLogDebug(NF_LOG_SYSTEMLOG, 0, "server:{} connect master success!", pConfig->ServerName);
+
+        if (!m_pObjPluginManager->IsInited())
+        {
+            RegisterMasterServer(proto_ff::EST_INIT);
+        } else
+        {
+            RegisterMasterServer(proto_ff::EST_NARMAL);
+        }
+
+        //完成服务器启动任务
+        if (!m_pObjPluginManager->IsInited())
+        {
+            m_pObjPluginManager->FinishAppTask(m_serverType, APP_INIT_CONNECT_MASTER);
+        }
+    } else if (nEvent == eMsgType_DISCONNECTED)
+    {
+        NFLogError(NF_LOG_SYSTEMLOG, 0, "server:{} disconnect master success", pConfig->ServerName);
+    }
+    return 0;
+}
+
+/*
+	处理Master服务器未注册协议
+*/
+int NFTransMsgServerModule::OnHandleMasterOtherMessage(uint64_t unLinkId, NFDataPackage &packet)
+{
+    NFLogWarning(NF_LOG_SYSTEMLOG, 0, "master server other message not handled:msgId:{}", packet.ToString());
+    return 0;
+}
+
+int NFTransMsgServerModule::OnHandleServerReportFromMasterServer(uint64_t unLinkId, NFDataPackage &packet)
+{
+    proto_ff::ServerInfoReportList xMsg;
+    CLIENT_MSG_PROCESS_NO_PRINTF(packet, xMsg);
+
+    for (int i = 0; i < xMsg.server_list_size(); ++i)
+    {
+        const proto_ff::ServerInfoReport &xData = xMsg.server_list(i);
+        switch (xData.server_type())
+        {
+            default:
+            {
+                OnHandleOtherServerReportFromMasterServer(xData);
+            }
+                break;
+        }
+    }
+    return 0;
+}
+
+int NFTransMsgServerModule::OnHandleOtherServerReportFromMasterServer(const proto_ff::ServerInfoReport &xData)
+{
+    return 0;
+}
+
+int NFTransMsgServerModule::ServerReportToMasterServer()
+{
+    NFServerConfig *pConfig = FindModule<NFIConfigModule>()->GetAppConfig(m_serverType);
+    if (pConfig)
+    {
+        proto_ff::ServerInfoReportList xMsg;
+        proto_ff::ServerInfoReport *pData = xMsg.add_server_list();
+        NFServerCommon::WriteServerInfo(pData, pConfig);
+        pData->set_server_state(proto_ff::EST_NARMAL);
+
+        NFIMonitorModule *pMonitorModule = m_pObjPluginManager->FindModule<NFIMonitorModule>();
+        if (pMonitorModule)
+        {
+            const NFSystemInfo &systemInfo = pMonitorModule->GetSystemInfo();
+            NFServerCommon::WriteServerInfo(pData, systemInfo);
+        }
+
+        if (pData->proc_cpu() > 0 && pData->proc_mem() > 0)
+        {
+            FindModule<NFIServerMessageModule>()->SendMsgToMasterServer(m_serverType, proto_ff::NF_SERVER_TO_MASTER_SERVER_REPORT, xMsg);
+        }
+    }
+    return 0;
+}
---
+++ src/NFServerComm/NFServerCommon/NFTransMsgServerModule.h
@@ -0,0 +1,133 @@
+// -------------------------------------------------------------------------
+//    @FileName         :    NFTransMsgServerModule.h
+//    @Author           :    gaoyi
+//    @Date             :    22-10-28
+//    @Email			:    445267987@qq.com
+//    @Module           :    NFTransMsgServerModule
+//
+// -------------------------------------------------------------------------
+
+#include "NFComm/NFPluginModule/NFIDynamicModule.h"
+
+/**
+ * @brief 服务器类似NFProxyServer,NFProxyAgentServer,NFRouteAgentServer，实现类似连接NFMasterServer等功能
+ */
+class NFTransMsgServerModule : public NFIDynamicModule
+{
+public:
+    NFTransMsgServerModule(NFIPluginManager *p, NF_SERVER_TYPES serverType) : NFIDynamicModule(p), m_serverType(serverType)
+    {
+        m_connectMasterServer = true;
+    }
+
+    virtual ~NFTransMsgServerModule()
+    {
+
+    }
+
+    /**
+     * @brief 处理定时器
+     * @param nTimerID
+     */
+    virtual void OnTimer(uint32_t nTimerID) override;
+
+    /**
+     * @brief 响应注册事件
+     * @param nEventID
+     * @param nSrcID
+     * @param bySrcType
+     * @param message
+     * @return
+     */
+    virtual int OnExecute(uint32_t nEventID, uint64_t nSrcID, uint32_t bySrcType, const google::protobuf::Message& message) override;
+public:
+    int BindServer();
+    int ConnectMasterServer();
+public:
+    //////////////////////////////////////////////////////////Server服务器//////////////////////////////////////////////////////////////////
+    /**
+     * @brief Server服务器连接事件，可以是网络/bus的连接事件
+     * @param nEvent
+     * @param unLinkId
+     * @return
+     */
+    virtual int OnServerSocketEvent(eMsgType nEvent, uint64_t unLinkId);
+
+    /**
+     * @brief 处理Server未注册的消息
+     * @param unLinkId
+     * @param packet
+     * @return
+     */
+    virtual int OnHandleServerOtherMessage(uint64_t unLinkId, NFDataPackage &packet);
+
+    /**
+     * @brief 处理Server服务器的连接掉线
+     * @param unLinkId
+     * @return
+     */
+    virtual int OnHandleServerDisconnect(uint64_t unLinkId);
+    //////////////////////////////////////////////////////////Server服务器//////////////////////////////////////////////////////////////////
+public:
+    //////////////////////////////////////////////////////////NFMasterServer服务器//////////////////////////////////////////////////////////////////
+    /**
+     * @brief 链接Master服务器
+     * @param xData
+     * @return
+     */
+    virtual int ConnectMasterServer(const proto_ff::ServerInfoReport &xData);
+
+    /**
+     * @brief 注册Master服务器
+     * @param serverState
+     * @return
+     */
+    virtual int RegisterMasterServer(uint32_t serverState);
+
+    /**
+     * @brief 处理Master服务器链接事件
+     * @param nEvent
+     * @param unLinkId
+     * @return
+     */
+    virtual int OnMasterSocketEvent(eMsgType nEvent, uint64_t unLinkId);
+
+    /**
+     * @brief 处理Master服务器未注册消息
+     * @param unLinkId
+     * @param packet
+     * @return
+     */
+    virtual int OnHandleMasterOtherMessage(uint64_t unLinkId, NFDataPackage &packet);
+
+    /**
+     * @brief 接受来自MasterServer的其他服务器的报告
+     * @param unLinkId
+     * @param packet
+     * @return
+     */
+    virtual int OnHandleServerReportFromMasterServer(uint64_t unLinkId, NFDataPackage &packet);
+
+    /**
+     * @brief 接受来自MasterServer的其他服务器的报告
+     * @param unLinkId
+     * @param packet
+     * @return
+     */
+    virtual int OnHandleOtherServerReportFromMasterServer(const proto_ff::ServerInfoReport &xData);
+
+    /**
+     * @brief 每隔一段时间向Master服务器发送自身信息
+     * @return
+     */
+    virtual int ServerReportToMasterServer();
+    //////////////////////////////////////////////////////////NFMasterServer服务器//////////////////////////////////////////////////////////////////
+public:
+    bool IsConnectMasterServer() const;
+
+    void SetConnectMasterServer(bool connectMasterServer);
+private:
+    NF_SERVER_TYPES m_serverType;
+    bool m_connectMasterServer;
+};
+
