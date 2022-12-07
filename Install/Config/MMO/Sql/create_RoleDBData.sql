f6c017de60fac9b3bbe468fef4408acbd83c90a5
gaoyi445 <445267987@qq.com>
2022-12-07 17:39:34 +0800

    new excel

--- Install/Config/MMO/Sql/create_RoleDBData.sql
+++ Install/Config/MMO/Sql/create_RoleDBData.sql
@@ -6,12 +6,16 @@ CREATE TABLE IF NOT EXISTS RoleDBData (
  bag BLOB /*!99104 COMPRESSED */ ,
  equip BLOB /*!99104 COMPRESSED */ ,
  task BLOB /*!99104 COMPRESSED */ ,
- unlockInfo BLOB /*!99104 COMPRESSED */ ,
+ unlockinfo BLOB /*!99104 COMPRESSED */ ,
  grow BLOB /*!99104 COMPRESSED */ ,
  mini BLOB /*!99104 COMPRESSED */ ,
  facade BLOB /*!99104 COMPRESSED */ ,
  skill BLOB /*!99104 COMPRESSED */ ,
  mini_activity BLOB /*!99104 COMPRESSED */ ,
+ dup BLOB /*!99104 COMPRESSED */ ,
+ buff BLOB /*!99104 COMPRESSED */ ,
+ boss BLOB /*!99104 COMPRESSED */ ,
+ vip BLOB /*!99104 COMPRESSED */ ,
  INDEX(zid),
  INDEX(uid),
  PRIMARY KEY(cid))  ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -23,10 +27,14 @@ alter table RoleDBData add column base BLOB ;
 alter table RoleDBData add column bag BLOB ;
 alter table RoleDBData add column equip BLOB ;
 alter table RoleDBData add column task BLOB ;
-alter table RoleDBData add column unlockInfo BLOB ;
+alter table RoleDBData add column unlockinfo BLOB ;
 alter table RoleDBData add column grow BLOB ;
 alter table RoleDBData add column mini BLOB ;
 alter table RoleDBData add column facade BLOB ;
 alter table RoleDBData add column skill BLOB ;
 alter table RoleDBData add column mini_activity BLOB ;
+alter table RoleDBData add column dup BLOB ;
+alter table RoleDBData add column buff BLOB ;
+alter table RoleDBData add column boss BLOB ;
+alter table RoleDBData add column vip BLOB ;
 */
